db444f7b1c259313c0afac0e3e9cf72e0e4164cb
MatePiranishvili <matepiranishvili6@gmail.com>
2024-05-10 13:41:12 +0400

    commit

---
+++ day 15/homework.py/homework.py
@@ -0,0 +1,20 @@
+number = int(input("enter your number : "))
+if number == 1:
+    print("that is odd number")
+elif number == 3:
+    print(" that is odd number")
+elif number == 5:
+    print(" that is odd number")
+elif number == 7:
+    print(" that is odd number")
+elif number == 2:
+    print("that is even numbe")
+elif number == 8:
+    print("that is even number")
+elif number == 11 :
+    print("that is odd number")
+else:
+    print("ERROR you picked number that is not on the list")
+
+
+
