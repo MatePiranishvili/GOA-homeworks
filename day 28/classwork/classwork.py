e8e1098db6a799e277f4c95c74cf88f879b3a5de
MatePiranishvili <matepiranishvili6@gmail.com>
2024-08-23 17:51:51 +0400

    commit

---
+++ day 28/classwork/classwork.py
@@ -0,0 +1,140 @@
+#1
+# for i in range(0,20):
+#     print(i)
+
+
+
+
+#2
+# num1=int(input("enter your number:"))
+
+# num2=int(input("enter your number:"))
+
+
+# for i in range(num1,num2):
+#     print(i)
+
+
+
+#3
+# for i in range(50,101):
+#     print(i)
+
+
+
+
+#4
+# for i in range(100,49,-1):
+#     print(i)
+
+
+
+
+#5
+# num1=0
+# for i in range(0,51):
+#     num1+=i
+
+
+
+
+#6
+# num1=int(input("please enter your number:"))
+
+# for i in range(0,num1):
+#     print(i)
+
+
+
+#7
+# num1=int(input("please enter your age:"))
+# num2=num1+10
+# for i in range(num1,num2):
+#     print(i)
+
+
+
+#8
+# num1=int(input("enter you number:"))
+# num2=int(input("enter your number:"))
+# print(num1+num2)
+# print(num1%num2)
+# print(num1*num2)
+# print(num1-num2)
+
+
+#9
+# num1=int(input("enter your age:"))
+# if num1 > 18:
+#     print("you cant let him in ")
+# else:
+#     print("let him in")
+
+
+
+#10
+# print(5000>1223)#True
+# print(6502<30)#False
+# print(780==780)#True
+# print(340<=232)#False
+# print(400>=400)#True
+# print(442!=232)#True
+
+
+
+#11
+# num1=int(input("please enter your number:"))
+# num2=int(input("please enter your numebr:"))
+# print(num1>num2)#False
+
+
+#12
+# num1=int(input("please enter you number 1-7:"))
+# if num1 == 1:
+#     print("monday")
+# elif num1 == 2:
+#     print("Tuesday")
+# elif num1 == 3:
+#     print("wednesday")
+# elif num1 == 4:
+#     print("thursday")
+# elif num1 == 5:
+#     print("friday")
+# elif num1 == 6:
+#     print("saturday")
+# elif num1 == 7:
+#     print("sunday")
+# else:
+#     print("weeks other day")
+
+#13
+# num=int(input("please enter your number 0-10:"))
+# if (num % 2) == 0:
+#     print("the number is even")
+# else:
+#     print("number is odd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
