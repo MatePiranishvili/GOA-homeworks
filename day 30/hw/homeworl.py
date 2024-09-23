da1089684a7e1e621ef86c7ce161ceb565579468
MatePiranishvili <matepiranishvili6@gmail.com>
2024-09-23 18:01:24 +0400

    commit

---
+++ day 30/hw/homeworl.py
@@ -0,0 +1,49 @@
+# def number():
+#    num=int(input("please neter your number:"))
+#    if (num % 2) == 0 :
+#       print("number is even")
+#    else:
+#       print("number is odd")
+# number()
+    
+    #2  
+# num=int(input("please eneter your number:"))
+# if (num  % 2):
+#      print("the number is negative")
+# else:
+#      print("the number is  positive")
+
+     #3
+# num=int(input("please enter your number"))
+# if (num % 5):
+#     print("number can be devied by 5 ")
+# else:
+#     print("number cant be devided by 5")
+    #4
+
+
+# num1=int(input("please enter your age:"))
+# if num1 < 18 :
+#     print("person is below 18 ")
+# elif num1 > 18:
+#     print("age is ebow 18")
+     #5
+
+
+
+#6
+
+
+# password = int(input("please neter your password:"))
+# if password < 8:
+#     print("registration failure")
+# elif password == 8:
+#     print("you succsefuly registered ")
+    
+
+   
+
+
+
+
+    
